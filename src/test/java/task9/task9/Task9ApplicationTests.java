72f2043d7b21f5931321c9e16f999269aefe43ab
LMMSProject <karrde54@gmail.com>
2021-01-28 13:51:32 +0800

    Task 9 initial commit

---
+++ src/test/java/task9/task9/Task9ApplicationTests.java
@@ -0,0 +1,13 @@
+package task9.task9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
